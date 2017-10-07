08c11815a7b0eeb28e751e1af8b090a31cbe35b6
arnewohletz1 <arnewohletz@gmx.de>
2017-10-07 13:59:24 +0200

    made data extractrion from first page possible

--- fun/gog.py
+++ fun/gog.py
@@ -5,27 +5,43 @@ from urllib.request import urlopen
 import json
 import re
 
+file = open("content.txt", "wb")
 
-file = open("content.txt", "a")
-i = 1
+for i in range(1, 43, 1):
 
-url = "https://www.gog.com/games?sort=title&page=1"
+    url = "https://www.gog.com/games?sort=title&page=" + str(i)
+    print(url)
 
-context = ssl._create_unverified_context()
-content = urlopen(url, context=context).read()
+    context = ssl._create_unverified_context()
+    content = urlopen(url, context=context).read()
 
-soup = BeautifulSoup(content, "html.parser")
-data = soup.find('script', type='text/javascript').string
+    soup = BeautifulSoup(content, "html.parser")
+    data = soup.find('script', type='text/javascript').string
 
-data_regex = re.compile(r'var .* = ')
-data = re.split(data_regex, data)
+    data_regex = re.compile(r'var .* = ')
+    data = re.split(data_regex, data)
 
-mydata = data[1].replace("};", "}")
+    mydata = data[1].replace("};", "}")
 
-my_json_data = json.loads(mydata)
+    my_json_data = json.loads(mydata)
 
-for i in range (49):
-    file.write(my_json_data['products'][i]['title'] + "\n")
+    for j in range(0, len(my_json_data['products']), 1):
+        title = my_json_data['products'][j]['title'].encode("utf-8")
+        print(type(title))
+        file.write(title + "\n".encode("utf-8"))
+
+    # PROBLEM:
+    # The title values are not transmitted via Beatiful Soup because
+    # these data is stored in a JavaScript Object. This however, this
+    # object somehow does only contain the data for the first site
+    # (it must be some kind of template) not the other forty. When
+    # running these URLs, the title data is neither inside the html nor
+    # inside the JS object.
+    # If the site uses the html as template and inputs the data from the JS
+    # object, I wonder where it gets the data from, since it is not in the Object.
+    # If the JS Object itself is the template, then why don't I get the values
+    # from the html?
+    # Find out, how this works before continuing!
 
 
 
