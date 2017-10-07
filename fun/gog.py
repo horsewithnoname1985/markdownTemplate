478653e58bc4d1155489be4c2ef2622e31d096c3
arnewohletz <arnewohletz@gmx.de>
2017-10-07 12:10:45 +0200

    added requirements.txt

--- fun/gog.py
+++ fun/gog.py
@@ -1,6 +1,5 @@
 #! python3
 from bs4 import BeautifulSoup
-import requests
 import ssl
 from urllib.request import urlopen
 import json
@@ -25,7 +24,9 @@ mydata = data[1].replace("};", "}")
 
 my_json_data = json.loads(mydata)
 
-#TODO: my_json_data is now a dictionary. Find a way to get all the 'title' value of all 'product' entries
+for i in range (49):
+    file.write(my_json_data['products'][i]['title'] + "\n")
+
 
 
 # TODO: Find a way to extract desired information from a javascript object containing data in json syntax
@@ -41,26 +42,3 @@ my_json_data = json.loads(mydata)
 #
 # see here: https://www.w3schools.com/js/js_json.asp
 # Help: https://stackoverflow.com/questions/8049520/web-scraping-javascript-page-with-python
-
-
-
-
-
-for i in range(1, 44, 1):
-    page = requests.get("https://www.gog.com/games?sort=title&page=" + str(i))
-    page.raise_for_status()
-    gog_soup = bs4.BeautifulSoup(page., "html.parser")
-    gog_soup = bs4.BeautifulSoup(page.text, "html.parser")
-    elems = gog_soup.select("product-title")
-    i = i + 1
-    j = 0
-    for j in range(len(elems)):
-        file.write(elems[j].getText())
-        j = j + 1
-file.close()
-
-
-
-
-
-text = gog_soup.find_all("span", attrs={"class": "product-title__text"})
