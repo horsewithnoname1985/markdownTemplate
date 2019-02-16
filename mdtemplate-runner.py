dbc3bfb87519c189e84368c5b2d9e1c696cf4328
arnewohletz1 <arnewohletz@gmx.de>
2019-02-16 07:27:29 +0100

    added mdtemplate-runner.py

---
+++ mdtemplate-runner.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""Convenience wrapper for running bootstrap directly from source tree"""
+
+from create_form import main
+
+if __name__ == "__main__":
+    main()
