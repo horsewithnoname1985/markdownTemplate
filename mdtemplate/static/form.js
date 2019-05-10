8a8ebccaf68652a8a6fcca0b36f6a6461c5ecaf2
horsewithnoname1985 <arnewohletz@gmx.de>
2019-05-10 16:50:15 +0200

    added chapter titles to HTML

--- mdtemplate/static/form.js
+++ mdtemplate/static/form.js
@@ -1,10 +1,14 @@
 function init() {
   var submitButton = document.getElementById("submit");
+  var addChapterTitleButton = document.getElementById("add_chapter_title");
+
   submitButton.onclick = checkMandatoryFields;
+  addChapterTitleButton.onclick = addChapterTitle;
 }
 
 
 function checkMandatoryFields(e) {
+//TODO: Fix this function
   var mandatoryFields = document.getElementsByClassName("required");
   for (var i = 0; i < mandatoryFields.length; i++) {
     if (mandatoryFields[i].value === "") {
@@ -16,5 +20,27 @@ function checkMandatoryFields(e) {
     }
   }
 }
+
+/**
+ * @param {String} HTML representing a single element
+ * @return {Element}
+ */
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+function addChapterTitle() {
+  var template = document.querySelector("#chapter_title");
+  var clone = document.importNode(template.content, true);
+
+  var chapter_table = document.querySelector("#chapter_table");
+  chapter_table.appendChild(clone);
+// create new ChapterTitle element
+// add element to correct position in HTML
+}
+
 
 window.onload = init;
